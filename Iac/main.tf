ccabfd28bf1adfa3550a7d684e1c382315dae159
Tejasri-19 <129884364+Tejasri-19@users.noreply.github.com>
2025-05-21 15:54:27 +0530

    Update main.tf

--- Iac/main.tf
+++ Iac/main.tf
@@ -1,16 +1,11 @@
 resource "aws_instance" "demo" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
-  instance_type = "t2.micro"
-  key_name      = "us-east-1-keypair"
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo amazon-linux-extras install nginx1 -y
-    sudo systemctl enable nginx
-    sudo systemctl start nginx
-    echo "<h1>Deployed via Terraform user_data</h1>" | sudo tee /usr/share/nginx/html/index.html
-  EOF
+  user_data = file("${path.module}/user_data.sh")
 
   tags = {
     Name = "harness-demo-instance"
