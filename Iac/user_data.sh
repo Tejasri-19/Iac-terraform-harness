994dc52ff666c1cc91d67600f3e1002844875155
Tejasri-19 <129884364+Tejasri-19@users.noreply.github.com>
2025-05-21 12:52:04 +0530

    Update user_data.sh

--- Iac/user_data.sh
+++ Iac/user_data.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-echo "Running user data script to install NGINX..." > /var/tmp/init.log
-sudo apt update -y
-sudo apt install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-echo "NGINX started via user data script" >> /var/tmp/init.log
 
